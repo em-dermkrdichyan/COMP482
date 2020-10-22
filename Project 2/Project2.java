01f3aada2200d5a29083c0fe951452a673d77df4
Emmanoel Dermkrdichyan <emmanoel.dermkrdichyan.584@my.csun.edu>
2020-10-22 14:49:30 -0700

    added L1 measurement feature

--- Project 2/Project2.java
+++ Project 2/Project2.java
@@ -22,6 +22,8 @@ public class Project2 {
         int smallestX = scanner.nextInt(),largestX = 0;
         int smallestY = scanner.nextInt(), largestY = 0;
         int counter = 0, first = smallestX, second = smallestY;
+        Point midPoint;
+
 
         while(scanner.hasNextInt()) {
             counter++;
@@ -30,7 +32,7 @@ public class Project2 {
                 second = scanner.nextInt();
             }
 
-            System.out.println("first: " + first + " second: " + second);
+            //System.out.println("first: " + first + " second: " + second);
             Point location = new Point(first, second);
             points.add(location);
 
@@ -49,19 +51,95 @@ public class Project2 {
             }
         }
 
-        //points.get(0).getX();
-        System.out.println("sX: " + smallestX + " lX: " + largestX + " sY: " + smallestY + " lY: " + largestY);
-
-        for (int i = 0; i < points.size(); i++) {
+        /*for (int i = 0; i < points.size(); i++) {
             System.out.println(points.get(i));
-        }
-        System.out.println(points);
+        }*/
+        //System.out.println(points);
+
+        //midPoint = findMid(points);
+        stepThrough(points, smallestX, smallestY, largestX, largestY);
+        //System.out.println(midPoint);
     }
 
+    static void stepThrough(ArrayList<Point> points, int sx, int sy, int lx, int ly) {
+        Point bestLOne = new Point (-1, -1);
+        Point bestLTwo = new Point (-1, -1);;
+        int bestDist = -1;
+
+        for (int x = sx; x <= lx; x++) {
+            for (int y = sy; y <= ly; y++) {
+                Point passingPoint = new Point (x, y);
+                int distOne = calcLOne(points, passingPoint);
+                
+                if (bestDist == -1) {
+                    bestDist = distOne;
+                    bestLOne.setLocation(passingPoint);
+                    //System.out.println("\n\npassingPT: " + passingPoint + "\n\n");
+                }
+
+                if (distOne < bestDist) {
+                    bestDist = distOne;
+                    //System.out.println("\n\npassingPT: " + passingPoint + "\n\n");
+                    bestLOne.setLocation(passingPoint);
+                }
 
 
+            }
 
+        }
 
+        System.out.println("The best loc: " + bestLOne + " dist: " + bestDist);
 
+    }
+
+    static int calcLOne (ArrayList<Point> points, Point passedIn) {
+        int addedDist = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            int tempDist = 0;
+            int tempX = 0;
+            int tempY = 0;
+
+            tempX = points.get(i).x - passedIn.x;
+            tempY = points.get(i).y - passedIn.y;
+
+            if (tempX < 0) {
+                tempX = tempX * -1;
+            }
+            if (tempY < 0) { 
+                tempY = tempY * -1;
+            }
+
+            tempDist = tempX + tempY;
+            addedDist = addedDist + tempDist;            
+        }
+        return addedDist;
+    }
+
+    static int calcLTwo (ArrayList<Point> points, Point passedIn) {
+
+        return 1;
+    }
+
+    static Point findMid(ArrayList<Point> points) {
+        double xNums = 0, yNums = 0;
+        double xMid = 0, yMid = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            xNums = xNums + points.get(i).x;
+            yNums = yNums + points.get(i).y;
+        }
+        System.out.println("xNum: " + xNums + " Y " + yNums + " size " + points.size());
+
+        xMid = xNums / points.size();
+        yMid = yNums / points.size();
+
+        System.out.println("\n\n XM: " + xMid + " YM: " + yMid);
+
+        Point midPoint = new Point();
+        midPoint.setLocation(xMid, yMid);
+
+        return midPoint; 
+    }
 
 }
